32fbc75578e61134583d5f55d6841984d5bdac44
Jeff Vazquez <jeffvazquez@cpp.edu>
2023-10-24 15:04:00 -0700

    Adding photogallery to github

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.kt
@@ -0,0 +1,63 @@
+package com.bignerdranch.android.photogallery
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bignerdranch.android.photogallery.databinding.FragmentPhotoPageBinding
+
+
+class PhotoPageFragment: Fragment() {
+    private val args: PhotoPageFragmentArgs by navArgs()
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentPhotoPageBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        binding.apply {
+            progressBar2.max=100
+
+            webView.apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl(args.photoPageUri.toString())
+
+                webChromeClient = object : WebChromeClient() {
+                    override fun onProgressChanged(
+                        webView: WebView,
+                        newProgress: Int
+                    ) {
+                        if (newProgress == 100) {
+                            progressBar2.visibility = View.GONE
+                        } else {
+                            progressBar2.visibility = View.VISIBLE
+                            progressBar2.progress = newProgress
+                        }
+                    }
+
+                    override fun onReceivedTitle(
+                        view: WebView?,
+                        title: String?
+                    ) {
+                        val parent = requireActivity() as AppCompatActivity
+                        parent.supportActionBar?.subtitle = title
+                    }
+                }
+            }
+        }
+        return binding.root
+    }
+}
